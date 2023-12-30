cbb3704f7a0809242c4ef68e6b75accb097c399c
rafsun-riad <mruhaquer@gmail.com>
2023-12-31 01:55:43 +0600

    finished

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -3,22 +3,33 @@ import TransactionMenu from './TransactionMenu';
 
 function Balance() {
   const [balance, setBalance] = useState(0);
-  const [Income, setIncome] = useState(1);
+  const [income, setIncome] = useState(0);
+  const [expense, setExpense] = useState(0);
+
+  function handleIncome(amount) {
+    setIncome((income) => income + amount);
+    setBalance((balance) => balance + amount);
+  }
+
+  function handleExpense(amount) {
+    setExpense((expense) => expense + amount);
+    setBalance((balance) => balance - amount);
+  }
 
   return (
     <div className="flex flex-col items-center gap-2">
       <p className="text-2xl mt-16">
-        Balance: <span className="font-bold">10000</span>
+        Balance: <span className="font-bold">{balance}</span>
       </p>
       <div className="flex flex-row space-x-8 mt-5 text-white">
         <p className="bg-green-800 p-2 rounded-xl">
-          Income: <span className="font-bold">1000</span>
+          Income: <span className="font-bold">{income}</span>
         </p>
         <p className="bg-red-500 p-2 rounded-xl">
-          Expense: <span className="font-bold">1000</span>
+          Expense: <span className="font-bold">{expense}</span>
         </p>
       </div>
-      <TransactionMenu />
+      <TransactionMenu onIncome={handleIncome} onExpense={handleExpense} />
     </div>
   );
 }
--- src/components/TransactionMenu.js
+++ src/components/TransactionMenu.js
@@ -1,5 +1,100 @@
-function TransactionMenu() {
-  return <div></div>;
+import { useState } from 'react';
+
+function TransactionMenu({ onIncome, onExpense }) {
+  const [menu, setMenu] = useState(false);
+  const [data, setData] = useState([]);
+  const [amount, setAmount] = useState('');
+  const [title, setTitle] = useState('');
+  const [transactionType, setTransactionType] = useState('expense');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!amount || !title) return;
+
+    const info = {
+      id: data.length + 1,
+      amount,
+      title,
+      transactionType,
+    };
+
+    if (transactionType === 'income') {
+      onIncome(Number(amount));
+    } else {
+      onExpense(Number(amount));
+    }
+
+    setData((data) => [...data, info]);
+    setAmount('');
+    setTitle('');
+    setTransactionType('expense');
+    setMenu(!menu);
+  }
+
+  return (
+    <div>
+      <div className="mt-6 flex flex-col items-center">
+        <button
+          className="bg-black text-white p-2 rounded-lg hover:bg-gray-700 mb-6"
+          onClick={() => setMenu(!menu)}
+        >
+          Add Transaction
+        </button>
+
+        {menu && (
+          <div className="flex items-center">
+            <div className="flex flex-col text-black gap-2">
+              <input
+                required
+                type="number"
+                placeholder="Enter Amount"
+                className="p-2 border rounded-lg border-black placeholder:text-gray-700"
+                value={amount}
+                onChange={(e) => setAmount(e.target.value)}
+              />
+              <input
+                required
+                type="text"
+                placeholder="Enter Title"
+                className="p-2 border rounded-lg border-black placeholder:text-gray-700"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+
+              <select
+                className="p-2 border rounded-lg"
+                value={transactionType}
+                onChange={(e) => setTransactionType(e.target.value)}
+              >
+                <option value="expense">Expense</option>
+                <option value="income">Income</option>
+              </select>
+              <button
+                onClick={handleSubmit}
+                className="bg-black text-white mb-2 p-0 rounded-lg hover:bg-gray-700 mt-3"
+              >
+                Add
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
+      {data.length > 0 &&
+        data.map((el) => (
+          <div className="flex" key={el.id}>
+            <div
+              className={`flex w-[200px] justify-between flex-row text-white gap-4 mb-2 p-2 rounded-md ${
+                el.transactionType === 'expense' ? 'bg-red-500' : 'bg-green-700'
+              }`}
+            >
+              <p>{el.title}</p>
+              <p>{el.amount}</p>
+            </div>
+          </div>
+        ))}
+    </div>
+  );
 }
 
 export default TransactionMenu;
